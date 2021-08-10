5e5dcfa3b8e4301de2a7f5634b9628f32afe7d41
adelina2222 <adelinachiriac@gmail.com>
2021-08-10 14:57:12 +0100

    Read the current directory & print the filenames in it

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Require the File System module
 const fs = require("fs");
+
+fs.readdir(".", (err, filenames) => {
+    if (err) {
+        console.log(err);
+    }
+    
+    console.log(filenames);
+});
