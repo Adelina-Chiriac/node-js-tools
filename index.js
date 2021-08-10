9bf84d9f6d5b1cf966c95c18066839a24765b437
adelina2222 <adelinachiriac@gmail.com>
2021-08-10 15:37:33 +0100

    Replace the cwd dot notation with the process.cwd function

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Require the File System module
 const fs = require("fs");
 
-fs.readdir(".", (err, filenames) => {
+fs.readdir(process.cwd(), (err, filenames) => {
     if (err) {
         console.log(err);
     }
-    
+
     console.log(filenames);
 });
